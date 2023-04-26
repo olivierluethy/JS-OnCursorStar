36db70875b7a1d3f1eb6db70fb4826e6592cbffb
Olivier_Luethy <olivier@kauz.ch>
2023-04-26 14:30:30 +0200

    Finished for now by providing lots of comments

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# JS-OnCursorStar
+# Cursor Info Animate
+This JavaScript code adds a number to the screen that follows the mouse cursor when it hovers over an element with the class “cursorInfoAnimate”. The number toggles between 0 and 1 and is displayed in a new <div> element that is created and appended to the selected element. The <div> element is removed after a delay of 750 milliseconds.
+
+## Usage
+To use this code, simply include it in your HTML file or JavaScript module. You can customize the appearance of the number by modifying the CSS properties of the “number” class.
+
+## Dependencies
+This code requires no external dependencies.
--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const myClass = document.querySelector(".cursorInfoAnimate");
-    let counter = 0;
-    let mouseX = 0;
-    let mouseY = 0;
+    const myClass = document.querySelector(".cursorInfoAnimate"); // Selects the element with the class "cursorInfoAnimate"
+    let counter = 0; // Initializes the counter variable to 0
+    let mouseX = 0; // Initializes the mouseX variable to 0
+    let mouseY = 0; // Initializes the mouseY variable to 0
 
-    myClass.addEventListener('mousemove', function(event) {
-        mouseX = event.clientX - myClass.offsetLeft;
-        mouseY = event.clientY - myClass.offsetTop;
+    myClass.addEventListener('mousemove', function(event) { // Adds an event listener for mousemove events on the selected element
+        mouseX = event.clientX - myClass.offsetLeft; // Calculates the x-coordinate of the mouse relative to the selected element
+        mouseY = event.clientY - myClass.offsetTop; // Calculates the y-coordinate of the mouse relative to the selected element
     });
 
-    setInterval(() => {
-      if(myClass.matches(':hover')){
-        const displayedNumber = document.createElement('div');
-        displayedNumber.className = 'number';
-        displayedNumber.innerHTML = counter;
-        displayedNumber.style.top = `${mouseY}px`;
-        displayedNumber.style.left = `${mouseX}px`;
-        myClass.appendChild(displayedNumber);
-  
-        // Toggle the counter between 0 and 1
-        counter = counter === 0 ? 1 : 0;
-  
-        setTimeout(() => {
-          displayedNumber.style.top = `${myClass.offsetHeight}px`;
-          setTimeout(() => {
-            displayedNumber.remove();
-          }, 750);
-        }, 1);
-      }
+    setInterval(() => { // Repeatedly executes a function at a specified interval (50 milliseconds in this case)
+        if (myClass.matches(':hover')) { // Checks if the selected element is being hovered over by the mouse
+            const displayedNumber = document.createElement('div'); // Creates a new <div> element
+            displayedNumber.className = 'number'; // Sets the class of the new element to "number"
+            displayedNumber.innerHTML = counter; // Sets the innerHTML of the new element to the value of the counter variable
+            displayedNumber.style.top = `${mouseY}px`; // Sets the top CSS property of the new element to the y-coordinate of the mouse
+            displayedNumber.style.left = `${mouseX}px`; // Sets the left CSS property of the new element to the x-coordinate of the mouse
+            myClass.appendChild(displayedNumber); // Appends the new element as a child of the selected element
+
+            // Toggle the counter between 0 and 1
+            counter = counter === 0 ? 1 : 0; // Toggles the value of the counter variable between 0 and 1
+
+            setTimeout(() => { // Executes a function after a specified delay (1 millisecond in this case)
+                displayedNumber.style.top = `${myClass.offsetHeight}px`; // Sets the top CSS property of the new element to be just below the bottom edge of the selected element
+                setTimeout(() => { // Executes a function after a specified delay (750 milliseconds in this case)
+                    displayedNumber.remove(); // Removes the new element from its parent
+                }, 750);
+            }, 1);
+        }
     }, 50);
 });
